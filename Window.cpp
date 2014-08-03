a63ec9edd06c6b6abe5f2698257f4d4bc1e131a5
SilentControl <robbfrostme@yahoo.com>
2014-08-03 21:28:38 +0300

    OOP-ed

--- Window.cpp
+++ Window.cpp
@@ -1,223 +1,157 @@
 #include <SDL.h>
 #include <cstdio>
-#include <bitset>
-
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int TILE_WIDTH = 32;
 const int TILE_HEIGHT = 32;
 
-SDL_Window* window = 0;
-SDL_Surface* screen = 0;
-SDL_Surface* image = 0;
-SDL_Surface* player = 0;
-SDL_Rect srctile; //= {23, 33, TILE_WIDTH, TILE_HEIGHT};
-SDL_Rect destile; //= {0, 0, TILE_WIDTH, TILE_HEIGHT};
-
-struct Direction
+struct GameShell
 {
-    int x;
-    int y;
+    SDL_Window* window;
+    SDL_Surface* screen;
+    SDL_Surface* background;
+
+    GameShell();
+    ~GameShell();
+    void loadMap();
+    void refresh();
 };
 
-void init()
+struct EventHandler
+{
+    SDL_Event event;
+    SDL_Rect direction;
+    bool quit;
+
+    EventHandler();
+    ~EventHandler();
+    void handleEvents();
+    void resetDirection();
+    bool exitGame();
+};
+
+EventHandler::EventHandler()
+{
+    direction.x = direction.y = 0;
+    quit = false;
+}
+
+EventHandler::~EventHandler()
+{
+}
+
+void EventHandler::resetDirection()
+{
+    direction.x = direction.y = 0;
+}
+
+void EventHandler::handleEvents()
+{
+    while(SDL_PollEvent(&event))
+    {
+        if(event.type == SDL_QUIT)
+        {
+            quit = true;
+        }
+
+        else
+        if(event.type == SDL_KEYDOWN)
+        {
+            switch(event.key.keysym.sym)
+            {
+                case SDLK_UP:
+                    direction.x = 0;
+                    direction.y = -1;
+                    break;
+                case SDLK_DOWN:
+                    direction.x = 0;
+                    direction.y = 1;
+                    break;
+                case SDLK_RIGHT:
+                    direction.x = 1;
+                    direction.y = 0;
+                    break;
+                case SDLK_LEFT:
+                    direction.x = -1;
+                    direction.y = 0;
+                    break;
+                case SDLK_q:
+                    quit = true;
+                    break;
+                default:
+                    direction.x = 0;
+                    direction.y = 0;
+                    break;
+            }
+        }
+    }
+}
+
+bool EventHandler::exitGame()
+{
+    return quit;
+}
+
+
+GameShell::GameShell()
 {
     SDL_Init(SDL_INIT_VIDEO);
     window = SDL_CreateWindow("sdl-alg-sd", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     screen = SDL_GetWindowSurface(window);
+    background = SDL_LoadBMP("cs2dnorm.bmp");
 }
 
-void loadMedia()
+GameShell::~GameShell()
 {
-    image = SDL_LoadBMP("cs2dnorm.bmp");
-    player = SDL_LoadBMP("hostages.bmp");
-    srctile.x = 0;
-    srctile.y = 5 * TILE_HEIGHT;
-    srctile.w = srctile.h = TILE_HEIGHT;
-
-    destile.x = 0;
-    destile.y = -TILE_WIDTH;
-
-    std::bitset<1> flag;
-    flag = 0;
-
-    for(int i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++)
-    {
-        if(i % 20 == 0)
-        {
-            destile.x = 0;
-            destile.y += TILE_HEIGHT;
-            flag = ~flag;
-        }
-
-        else
-        {
-            destile.x += TILE_WIDTH;
-        }
-
-        if(flag == 0)
-        {
-            if(i % 2 == 0)
-            {
-                srctile.x = 0;
-                srctile.y = 5 * TILE_HEIGHT;
-                srctile.w = srctile.h = TILE_HEIGHT;
-                SDL_BlitSurface(image, &srctile, screen, &destile);
-            }
-
-            else
-            {
-                srctile.x = 0;
-                srctile.y = 6 * TILE_HEIGHT;
-                srctile.w = srctile.h = TILE_HEIGHT;
-                SDL_BlitSurface(image, &srctile, screen, &destile);
-            }
-        }
-
-        else
-        {
-            if(i % 2 != 0)
-            {
-                srctile.x = 0;
-                srctile.y = 5 * TILE_HEIGHT;
-                srctile.w = srctile.h = TILE_HEIGHT;
-                SDL_BlitSurface(image, &srctile, screen, &destile);
-            }
-
-            else
-            {
-                srctile.x = 0;
-                srctile.y = 6 * TILE_HEIGHT;
-                srctile.w = srctile.h = TILE_HEIGHT;
-                SDL_BlitSurface(image, &srctile, screen, &destile);
-            }
-        }
-    }
-
-    srctile.x = srctile.y = 0;
-    destile.x = 9 * TILE_WIDTH;
-    destile.y = 14 * TILE_HEIGHT;
-
-    // transparency
-    Uint32 color = SDL_MapRGB(player->format, 0xff, 0x00, 0xff);
-    SDL_SetColorKey(player, SDL_TRUE, color);
-    SDL_BlitSurface(player, &srctile, screen, &destile);
-    SDL_UpdateWindowSurface(window);
-}
-
-void close()
-{
-    SDL_FreeSurface(image);
+    SDL_FreeSurface(background);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
+void GameShell::refresh()
+{
+    SDL_UpdateWindowSurface(window);
+}
+
+void GameShell::loadMap()
+{
+    SDL_Rect srctile, destile;
+    srctile.y = 5 * TILE_HEIGHT;
+    srctile.w = srctile.h = TILE_HEIGHT;
+
+    for (int i = 0; i < SCREEN_WIDTH / TILE_WIDTH; i++)
+    {
+        for (int j = 0; j < SCREEN_HEIGHT / TILE_HEIGHT; j++)
+        {
+            if ((i + j) % 2)
+                srctile.x =  5 * TILE_WIDTH;
+            else
+                srctile.x = 4 * TILE_WIDTH;
+
+            destile.x = i * TILE_WIDTH;
+            destile.y = j * TILE_HEIGHT;
+            SDL_BlitSurface(background, &srctile, screen, &destile);
+        }
+    }
+    SDL_UpdateWindowSurface(window);
+}
 
 int main(int argc, char* args[])
 {
-    init();
-    loadMedia();
+    GameShell game;
+    game.loadMap();
 
-    bool running = true;
-    SDL_Event event;
-    Direction direction;
-    direction.x = 0;
-    direction.y = 0;
-    Direction position;
-    position.x = 9 * TILE_WIDTH;
-    position.y = 14 * TILE_WIDTH;
-    while(running)
+    EventHandler actions;
+    while(true)
     {
-        while(SDL_PollEvent(&event))
+        actions.handleEvents();
+        if(actions.exitGame() == true)
         {
-            if(event.type == SDL_QUIT)
-            {
-                running = false;
-            }
-
-            else
-            if(event.type == SDL_KEYDOWN)
-            {
-                switch(event.key.keysym.sym)
-                {
-                    case SDLK_UP:
-                        direction.x = 0;
-                        direction.y = -1;
-                        break;
-                    case SDLK_DOWN:
-                        direction.x = 0;
-                        direction.y = 1;
-                        break;
-                    case SDLK_RIGHT:
-                        direction.x = 1;
-                        direction.y = 0;
-                        break;
-                    case SDLK_LEFT:
-                        direction.x = -1;
-                        direction.y = 0;
-                        break;
-                    default:
-                        direction.x = 0;
-                        direction.y = 0;
-                        break;
-                }
-            }
+            break;
         }
-
-        SDL_Rect tilecolor;
-        SDL_Rect playerpos;
-        if(direction.x != 0 || direction.y != 0)
-        {
-            if((position.y / TILE_HEIGHT) % 2 == 0)
-            {
-                if((position.x / TILE_WIDTH) % 2 == 0)
-                {
-                    tilecolor.x = 0;
-                    tilecolor.y = 6 * TILE_HEIGHT;
-                    tilecolor.w = tilecolor.h = TILE_WIDTH;
-                }
-
-                else
-                {
-                    tilecolor.x = 0;
-                    tilecolor.y = 5 * TILE_HEIGHT;
-                    tilecolor.w = tilecolor.h = TILE_WIDTH;
-                }
-            }
-
-            else
-            {
-                if((position.x / TILE_WIDTH) % 2 != 0)
-                {
-                    tilecolor.x = 0;
-                    tilecolor.y = 6 * TILE_HEIGHT;
-                    tilecolor.w = tilecolor.h = TILE_WIDTH;
-                }
-
-                else
-                {
-                    tilecolor.x = 0;
-                    tilecolor.y = 5 * TILE_HEIGHT;
-                    tilecolor.w = tilecolor.h = TILE_WIDTH;
-                }
-            }
-
-            playerpos.x = position.x;
-            playerpos.y = position.y;
-            printf("X = %d, Y = %d\n", position.x, position.y);
-            SDL_BlitSurface(image, &tilecolor, screen, &playerpos);
-            position.x += direction.x * TILE_WIDTH;
-            position.y += direction.y * TILE_HEIGHT;
-            playerpos.x = position.x;
-            playerpos.y = position.y;
-            srctile.x = srctile.y = 0;
-            srctile.w = srctile.h = TILE_WIDTH;
-            SDL_BlitSurface(player, &srctile, screen, &playerpos);
-            SDL_UpdateWindowSurface(window);
-            direction.x = direction.y = 0;
-        }
+        game.refresh();
+        actions.resetDirection();
     }
-    close();
 	return 0;
 }
