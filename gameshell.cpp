945a0c2b40b527c59a17abd5b5e2d4d3b5fc939d
SilentControl <robbfrostme@yahoo.com>
2014-08-04 23:52:30 +0300

    Added map

--- gameshell.cpp
+++ gameshell.cpp
@@ -1,12 +1,25 @@
 #include <SDL.h>
 #include "GameShell.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
 
 GameShell::GameShell()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("sdl-alg-sd", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN);
+    window = SDL_CreateWindow("sdl-alg-sd", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     screen = SDL_GetWindowSurface(window);
     background = SDL_LoadBMP("cs2dnorm.bmp");
+    SDL_Rect tile;
+    tile.y = 0;
+    tile.w = tile.h = TILE_HEIGHT;
+    tile.x = 10 * TILE_WIDTH;
+    bkgtiles.push_back(tile);
+    tile.x = 11 * TILE_WIDTH;
+    bkgtiles.push_back(tile);
+    tile.x = 14 * TILE_WIDTH;
+    tile.y = 6 * TILE_HEIGHT;
+    bkgtiles.push_back(tile);
 }
 
 GameShell::~GameShell()
@@ -24,38 +37,29 @@ void GameShell::refresh()
 void GameShell::loadMap()
 {
     SDL_Rect srctile, destile;
-    srctile.y = 5 * TILE_HEIGHT;
-    srctile.w = srctile.h = TILE_HEIGHT;
+    std::ifstream mapfile("map.txt");
+    int nr;
 
-    for (int i = 0; i < SCREEN_WIDTH / TILE_WIDTH; i++)
+    destile.x = -TILE_WIDTH;
+    destile.y = 0;
+    while(mapfile >> nr)
     {
-        for (int j = 0; j < SCREEN_HEIGHT / TILE_HEIGHT; j++)
+        tiles.push_back(nr);
+        srctile = bkgtiles[nr];
+        destile.x += TILE_WIDTH;
+        SDL_BlitSurface(background, &srctile, screen, &destile);
+        if(destile.x >= SCREEN_WIDTH - TILE_WIDTH)
         {
-            if ((i + j) % 2)
-                srctile.x =  5 * TILE_WIDTH;
-            else
-                srctile.x = 4 * TILE_WIDTH;
-
-            destile.x = i * TILE_WIDTH;
-            destile.y = j * TILE_HEIGHT;
-            SDL_BlitSurface(background, &srctile, screen, &destile);
+            destile.y += TILE_WIDTH;
+            destile.x = -TILE_WIDTH;
         }
     }
+    mapfile.close();
     SDL_UpdateWindowSurface(window);
 }
 
 void GameShell::repaintTile(SDL_Rect coord)
 {
-    SDL_Rect srctile;
-    srctile.y = 5 * TILE_HEIGHT;
-    srctile.w = srctile.h = TILE_HEIGHT;
-    if(((coord.x / TILE_WIDTH) + (coord.y / TILE_HEIGHT)) % 2)
-    {
-        srctile.x = 5 * TILE_WIDTH;
-    }
-    else
-    {
-        srctile.x = 4 * TILE_WIDTH;
-    }
-    SDL_BlitSurface(background, &srctile, screen, &coord);
+    // tiles[i]; i = 20 * Y + X;
+    SDL_BlitSurface(background, &bkgtiles[tiles[(coord.x / TILE_WIDTH) + 20 * (coord.y / TILE_HEIGHT)]], screen, &coord);
 }
